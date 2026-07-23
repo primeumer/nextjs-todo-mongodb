2121c89b8fdc0b4bcf890305851fa956c095b085
Umer Asif <umer.alwl989@gmail.com>
2026-07-23 20:04:35 +0500

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,13 @@
-export default function RootLayout({ children }){
-  return(
-<html lang="en">
-  <body>
-
-    {children}
-  </body>
-</html>
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
   );
 }
